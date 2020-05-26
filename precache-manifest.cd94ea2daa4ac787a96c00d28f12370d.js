301a09d139826fb794108f389987ace43a75d72b
Vivek-Kamble <vvek.kamble@gmail.com>
2020-05-26 13:00:52 +0530

    Updates

---
+++ precache-manifest.cd94ea2daa4ac787a96c00d28f12370d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0745a49b6ffd63e155936d1989a3b9ae",
+    "url": "/utilize-task/index.html"
+  },
+  {
+    "revision": "49b020b69474109f6679",
+    "url": "/utilize-task/static/css/main.f1534c3b.chunk.css"
+  },
+  {
+    "revision": "255299e3c21705a7305b",
+    "url": "/utilize-task/static/js/2.a99fa44b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/utilize-task/static/js/2.a99fa44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b020b69474109f6679",
+    "url": "/utilize-task/static/js/main.273a32fb.chunk.js"
+  },
+  {
+    "revision": "881d60eb729a39ab4651",
+    "url": "/utilize-task/static/js/runtime-main.8f8d4f2a.js"
+  }
+]);
